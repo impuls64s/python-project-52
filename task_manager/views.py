4df2d2513e61aeb7a8392990238f9614a0a1d6ff
Oleg Cherkashin <impuls_64@mail.ru>
2022-12-23 12:49:30 +0300

    bootstrap

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html', context={
-        'who': 'Asya & Oleg',
-    })
+    return render(request, 'index.html')
